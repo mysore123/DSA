14630ec302a7e845b81c348dc6fd17ec96999f8e
Anirudh Mysore <apmysore10@gmail.com>
2023-05-17 03:29:23 +0530

    added files to main

---
+++ src/strings/First.java
@@ -0,0 +1,7 @@
+package strings;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/trees/Third.java
@@ -0,0 +1,7 @@
+package trees;
+
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
