95d2e55e3068c138f73ee1f4f4d704ea7511f989
Anirudh Mysore <apmysore10@gmail.com>
2023-05-17 02:40:38 +0530

    First hello world file

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //Data Structures
+        System.out.println("Hello World");
+    }
+}
+us
